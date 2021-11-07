78a185fe02d046d680c33fe167370a96890ca6ae
banzaiMimic <2274050+dr-mimic@users.noreply.github.com>
2021-11-07 07:59:02 -0500

    omnisharp added for code formatting

--- Assets/scripts/player/playerFiniteStateMachine/PlayerState.cs
+++ Assets/scripts/player/playerFiniteStateMachine/PlayerState.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerState : MonoBehaviour
-{
-  // Start is called before the first frame update
-  void Start()
-  {
+public class PlayerState {
 
-  }
+  protected Player player;
+  protected PlayerStateMachine stateMachine;
+  protected SO_PlayerData playerData;
 
-  // Update is called once per frame
-  void Update()
-  {
-
-  }
 }
