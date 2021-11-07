0d39e39a3d432c877d454cc174d5b65fa0d5eb0f
banzaiMimic <2274050+dr-mimic@users.noreply.github.com>
2021-11-07 12:05:21 -0500

    fix double jump bug

--- Assets/scripts/player/input/PlayerInputHandler.cs
+++ Assets/scripts/player/input/PlayerInputHandler.cs
@@ -9,6 +9,15 @@ public class PlayerInputHandler : MonoBehaviour {
   public int normalizedInputX { get; private set; }
   public int normalizedInputY { get; private set; }
   public bool jumpInput { get; private set; }
+
+  [SerializeField]
+  private float inputHoldTime = 0.2f;
+
+  private float jumpInputStartTime;
+
+  private void Update() {
+    CheckJumpInputHoldTime();
+  }
   
   public void OnMoveInput(InputAction.CallbackContext context) {
     rawMovementInput = context.ReadValue<Vector2>();
@@ -19,9 +28,16 @@ public class PlayerInputHandler : MonoBehaviour {
   public void OnJumpInput(InputAction.CallbackContext context) {
     if (context.started) {
       jumpInput = true;
+      jumpInputStartTime = Time.time;
     }
   }
 
   public void UseJumpInput() => jumpInput = false;
+
+  private void CheckJumpInputHoldTime() {
+    if (Time.time >= jumpInputStartTime + inputHoldTime) {
+      jumpInput = false;
+    }
+  }
 
 }
