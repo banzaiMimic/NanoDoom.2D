2065c0fe44e7621e9ea35fa5c59b84aa61811d05
banzaiMimic <2274050+dr-mimic@users.noreply.github.com>
2021-11-21 17:49:02 -0500

    adding render pipeline

---
+++ Assets/scripts/movement/BloodSplatterMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodSplatterMovement : MonoBehaviour {
+  [SerializeField] private Vector2 parallaxEffectMultiplier = new Vector2(-.2f, .01f);
+  [SerializeField] public Transform cameraTransform;
+  private Vector3 lastCameraPosition;
+  private float textureUnitSizeX;
+
+  private void Start() {
+    Debug.Log("Parallax running");
+    lastCameraPosition = cameraTransform.position;
+    Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+    Texture2D texture = sprite.texture;
+    textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+  }
+
+  private void LateUpdate() {
+    Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+    transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+    transform.position = new Vector2(transform.position.x, -2.7f);
+    lastCameraPosition = cameraTransform.position;
+  }
+}
