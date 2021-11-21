1c9cebe011453b6c40fe560e9e83349f849a7453
banzaiMimic <2274050+dr-mimic@users.noreply.github.com>
2021-11-21 18:27:57 -0500

    lighting updates

--- Assets/scripts/movement/ParallaxBackground.cs
+++ Assets/scripts/movement/ParallaxBackground.cs
@@ -24,7 +24,7 @@ public class ParallaxBackground : MonoBehaviour {
 
     if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX) {
       float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
-      transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
+      transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y, transform.position.z);
     }
   }
 }
