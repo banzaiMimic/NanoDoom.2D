78dced7bfc307e4633120845f33c97faf91980e7
banzaiMimic <2274050+dr-mimic@users.noreply.github.com>
2021-11-25 21:25:00 -0500

    flying enemy working with collisions + movement + up down sine

--- Assets/scripts/enemies/enemySpecific/flying/EnemyFlying_MoveState.cs
+++ Assets/scripts/enemies/enemySpecific/flying/EnemyFlying_MoveState.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyFlying_MoveState : MoveState {
 
-
+  private float speedY = 3f;
+  private float startY = 0f;
+  private float maxTravelDistanceY = 3f;
   
   public EnemyFlying_MoveState(
     Entity entity, 
@@ -17,12 +20,19 @@ public class EnemyFlying_MoveState : MoveState {
   public override void Enter() {
     base.Enter();
     Debug.Log("Flying move state entered");
-    //core.Movement.SetVelocityX(stateData.movementSpeed * core.Movement.facingDirection);
+    startY = Mathf.Abs(entity.transform.position.y);
+    entity.core.Movement.SetVelocityY(-speedY);
   }
 
   public override void LogicUpdate() {
     base.LogicUpdate();
     // @TODO move vel y up and down
+    float travelDistanceY = startY - Mathf.Abs(entity.transform.position.y);
+    Debug.Log("Mathf.Abs(travelDistanceY): " + Mathf.Abs(travelDistanceY));
+    if (Mathf.Abs(travelDistanceY) >= maxTravelDistanceY) {
+      startY = Mathf.Abs(entity.transform.position.y);
+      entity.core.Movement.SetVelocityY(-entity.core.Movement.currentVelocity.y);
+    }
     //core.Movement.SetVelocityX(stateData.movementSpeed * core.Movement.facingDirection);
     // @TODO we need to be able to detect a trigger ? 
     // or attach a trigger to a collider...
