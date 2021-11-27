f232167b859829fa385e8d2beb3a981ee64c16aa
banzaiMimic <2274050+dr-mimic@users.noreply.github.com>
2021-11-26 23:53:16 -0500

    flying spawner decent enough where it works

--- Assets/scripts/spawners/SpawnFlying.cs
+++ Assets/scripts/spawners/SpawnFlying.cs
@@ -7,12 +7,9 @@ public class SpawnFlying : MonoBehaviour {
   [SerializeField] private GameObject flyingEnemy;
   [SerializeField] private Player player;
 
-  public float maxTime = 5;
-  public float minTime = 2;
-  //current time
+  public float maxTime = 6;
+  public float minTime = 3;
   private float time;
-
-  //The time to spawn the object
   private float spawnTime;
 
   void Start(){
@@ -21,21 +18,15 @@ public class SpawnFlying : MonoBehaviour {
   }
 
   void FixedUpdate(){
-
-    //Counts up
     time += Time.deltaTime;
-
     //Check if its the right time to spawn the object
     if(time >= spawnTime && player){
       SpawnObject();
       SetRandomTime();
     }
-
   }
 
-  //Spawns the object and resets the time
   void SpawnObject(){
-    Debug.Log("[SpawnFlying] -- spawning enemy flyer");
     time = minTime;
     float spawnY = player.core.Movement.rBody.transform.position.y + Random.Range(0f, 10f);
     float spawnX = player.core.Movement.rBody.transform.position.x + 20;
@@ -43,7 +34,6 @@ public class SpawnFlying : MonoBehaviour {
     Instantiate (flyingEnemy, spawnPoint , flyingEnemy.transform.rotation);
   }
 
-  //Sets the random time between minTime and maxTime
   void SetRandomTime(){
     spawnTime = Random.Range(minTime, maxTime);
   }
